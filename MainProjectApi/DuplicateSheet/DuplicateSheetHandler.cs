2f9917a286d312b6e1b4dd759948a1ebed567300
Nguyen van Phong <ngvanphong2012@gmail.com>
2019-08-10 09:23:58 +0700

    654

--- MainProjectApi/DuplicateSheet/DuplicateSheetHandler.cs
+++ MainProjectApi/DuplicateSheet/DuplicateSheetHandler.cs
@@ -24,52 +24,84 @@ namespace MainProjectApi.DuplicateSheet
             string endName = AppPanelDuplicateSheet.myFormDuplicateSheet.textBoxEndName.Text;
             foreach (var item in listSheetView)
             {
-                FamilyInstance titleblock = new FilteredElementCollector(doc).OfClass(typeof(FamilyInstance))
-                    .OfCategory(BuiltInCategory.OST_TitleBlocks).Cast<FamilyInstance>().First(q => q.OwnerViewId == item.Sheet.Id);
-                ViewSheet sheet = null;
-                using (Transaction t = new Transaction(doc, "DuplicateSheet"))
+                try
                 {
-                    t.Start();
-                    sheet = ViewSheet.Create(doc, titleblock.GetTypeId());
+                    FamilyInstance titleblock = new FilteredElementCollector(doc).OfClass(typeof(FamilyInstance))
+                    .OfCategory(BuiltInCategory.OST_TitleBlocks).Cast<FamilyInstance>().First(q => q.OwnerViewId == item.Sheet.Id);
+                    ViewSheet sheet = null;
+                    using (Transaction t = new Transaction(doc, "DuplicateSheet"))
+                    {
+                        t.Start();
+                        sheet = ViewSheet.Create(doc, titleblock.GetTypeId());
 
-                    sheet.SheetNumber = CompareString(item.Sheet.SheetNumber, endNumber);
-                    sheet.ViewName = CompareString(item.Sheet.ViewName, endName);
-                    t.Commit();
+                        sheet.SheetNumber = CompareString(item.Sheet.SheetNumber, endNumber);
+                        sheet.ViewName = CompareString(item.Sheet.ViewName, endName);
+                        t.Commit();
+                    }
+
+                    using (Transaction t2 = new Transaction(doc, "DupicateView"))
+                    {
+                        t2.Start();
+                        ViewDuplicateOption option = ViewDuplicateOption.WithDetailing;
+                        if (AppPanelDuplicateSheet.myFormDuplicateSheet.radioButtonDuplicate.Checked)
+                        {
+                            option = ViewDuplicateOption.Duplicate;
+                        }
+                        else if (AppPanelDuplicateSheet.myFormDuplicateSheet.radioButtonDuplicateasdepending.Checked)
+                        {
+                            option = ViewDuplicateOption.AsDependent;
+                        }
+
+                        foreach (var view in item.ListView)
+                        {
+                            Autodesk.Revit.DB.View viewDuplicate = null;
+                            ElementId id = view.Duplicate(option);
+                            viewDuplicate = doc.GetElement(id) as Autodesk.Revit.DB.View;
+                            //viewDuplicate.Name = CompareString(view.Name, endName);
+                            Viewport viewPortOld = getViewPort(doc, item.Sheet, view.Id).ViewPortRe;
+                            var location = viewPortOld.GetBoxCenter();
+                            Viewport viewNew = Viewport.Create(doc, sheet.Id, viewDuplicate.Id, location);
+                            viewNew.ChangeTypeId(viewPortOld.GetTypeId());
+                        }
+                        t2.Commit();
+                    }
+                    if (AppPanelDuplicateSheet.myFormDuplicateSheet.radioButtonLegendYes.Checked)
+                    {
+                        foreach (var legend in item.ListLegend)
+                        {
+                            using (Transaction t3 = new Transaction(doc, "AssignViewSheet"))
+                            {
+                                t3.Start();
+                                Viewport legendOld = getViewPort(doc, item.Sheet, legend.Id).ViewPortRe;
+                                var locationLegend = legendOld.GetBoxCenter();
+                                Viewport legendNew = Viewport.Create(doc, sheet.Id, legend.Id, locationLegend);
+                                legendNew.ChangeTypeId(legendOld.GetTypeId());
+                                t3.Commit();
+                            }
+                        }
+                    }
+                    if (AppPanelDuplicateSheet.myFormDuplicateSheet.radioButtonScheduleYes.Checked)
+                    {
+                        foreach (var schedule in item.ListSchedule)
+                        {
+                            using (Transaction t4 = new Transaction(doc, "AssignSchedule"))
+                            {
+                                t4.Start();
+                                ScheduleSheetInstance scheduleOld = getViewPort(doc, item.Sheet, schedule.Id).SheetSchedule;
+                                var locationSchedule = scheduleOld.Point;
+                                ScheduleSheetInstance scheduleNew = ScheduleSheetInstance.Create(doc, sheet.Id, schedule.Id, locationSchedule);
+                                t4.Commit();
+                            }
+                        }
+                    }
+                }
+                catch
+                {
+                    TaskDialog.Show("Error", "Sheet Number is existed");
+                    break;
                 }
                 
-                using (Transaction t2 = new Transaction(doc, "DupicateView"))
-                {
-                    t2.Start();
-                    ViewDuplicateOption option = ViewDuplicateOption.WithDetailing;
-                    if (AppPanelDuplicateSheet.myFormDuplicateSheet.radioButtonDuplicate.Checked)
-                    {
-                        option = ViewDuplicateOption.Duplicate;
-                    }
-                    else if (AppPanelDuplicateSheet.myFormDuplicateSheet.radioButtonDuplicateasdepending.Checked)
-                    {
-                        option = ViewDuplicateOption.AsDependent;
-                    }
-
-                    foreach (var view in item.ListView)
-                    {
-                        Autodesk.Revit.DB.View viewDuplicate = null;
-                        ElementId id = view.Duplicate(option);
-                        viewDuplicate = doc.GetElement(id) as Autodesk.Revit.DB.View;
-                        //viewDuplicate.Name = CompareString(view.Name, endName);
-                        Viewport viewPortOld = getViewPort(doc, item.Sheet, view.Id);
-                        var location = viewPortOld.GetBoxCenter();
-                        Viewport viewNew=Viewport.Create(doc, sheet.Id, viewDuplicate.Id, location);
-                        viewNew.ChangeTypeId(viewPortOld.GetTypeId());
-                    }
-                    t2.Commit();
-                }
-
-                using (Transaction t3 = new Transaction(doc, "AssignViewSheet"))
-                {
-                    t3.Start();
-
-                    t3.Commit();
-                }
+                
             }
 
         }
@@ -106,12 +138,8 @@ namespace MainProjectApi.DuplicateSheet
                         List<Autodesk.Revit.DB.View> listViewSheet = new List<Autodesk.Revit.DB.View>();
                         foreach (var item in listAllView)
                         {
-                            if (item.ViewType == ViewType.Schedule)
-                            {
-                                ViewSchedule schedule = item as ViewSchedule;
-                                listSchedule.Add(schedule);
-                            }
-                            else if (item.ViewType == ViewType.Legend)
+                          
+                            if (item.ViewType == ViewType.Legend)
                             {
                                 listLegend.Add(item);
                             }
@@ -119,6 +147,13 @@ namespace MainProjectApi.DuplicateSheet
                             {
                                 listViewSheet.Add(item);
                             }
+                        }
+                        var viewSchedules = new FilteredElementCollector(doc, sheet.Id).OfClass(typeof(ScheduleSheetInstance)).Cast<ScheduleSheetInstance>();
+                        foreach(var item in viewSchedules)
+                        {
+                            var id = item.ScheduleId;
+                            ViewSchedule schedule = doc.GetElement(id) as ViewSchedule;
+                            listSchedule.Add(schedule);
                         }
                         sheetView.ListView = listViewSheet;
                         sheetView.ListSchedule = listSchedule;
@@ -131,19 +166,30 @@ namespace MainProjectApi.DuplicateSheet
             return listResult;
         }
 
-        public Viewport getViewPort(Document doc, ViewSheet sheet, ElementId idView)
+        public ViewPortShechule getViewPort(Document doc, ViewSheet sheet, ElementId idView)
         {
-            Viewport result = null;
+            ViewPortShechule result = new ViewPortShechule();
+            var listScheduleSheet = new FilteredElementCollector(doc, sheet.Id).OfClass(typeof(ScheduleSheetInstance)).Cast<ScheduleSheetInstance>();
+            foreach(var schedule in listScheduleSheet)
+            {
+                if (schedule.ScheduleId == idView)
+                {
+                    result.SheetSchedule = schedule;
+                    return result;
+                }
+               
+            }
             var viewPortIds = sheet.GetAllViewports();
             foreach (var id in viewPortIds)
             {
-                Viewport viewPort = doc.GetElement(id) as Viewport;
-                if (viewPort.ViewId == idView)
-                {
-                    result = viewPort;
-                    return result;
-                }
+                    Viewport viewPort = doc.GetElement(id) as Viewport;
+                    if (viewPort.ViewId == idView)
+                    {
+                        result.ViewPortRe = viewPort;
+                        return result;
+                    } 
             }
+            
             return result;
 
         }
@@ -158,5 +204,10 @@ namespace MainProjectApi.DuplicateSheet
         public List<Autodesk.Revit.DB.View> ListView { set; get; }
         public List<Autodesk.Revit.DB.View> ListLegend { set; get; }
         public List<ViewSchedule> ListSchedule { set; get; }
+    }
+    public class ViewPortShechule
+    {
+        public Viewport ViewPortRe { set; get; }
+        public ScheduleSheetInstance SheetSchedule { get; set; }
     }
 }
