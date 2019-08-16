28d14aca1f9705d7a83032bb7724183febf136b0
Nguyen van Phong <ngvanphong2012@gmail.com>
2019-08-16 11:53:04 +0700

    658

--- ProjectApiV3/FilterElement/FilterElementBinding.cs
+++ ProjectApiV3/FilterElement/FilterElementBinding.cs
@@ -60,22 +60,5 @@ namespace ProjectApiV3.FilterElement
             }
         }
     }
-    class CategoryComparer : IEqualityComparer<Category>
-    {
-        #region Implementation of IEqualityComparer<in Category>
-
-        public bool Equals(Category x, Category y)
-        {
-            if (x == null || y == null) return false;
-
-            return x.Id.Equals(y.Id);
-        }
-
-        public int GetHashCode(Category obj)
-        {
-            return obj.Id.IntegerValue;
-        }
-
-        #endregion
-    }
+    
 }
