7a04d04a20bfb82ff61696a641f97078510da68d
Nguyen van Phong <ngvanphong2012@gmail.com>
2019-12-18 17:15:05 +0700

    6355

--- MainProjectApi/ViewSheetAsign/ViewToSheetHandler.cs
+++ MainProjectApi/ViewSheetAsign/ViewToSheetHandler.cs
@@ -4,18 +4,68 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Autodesk.Revit.UI;
+using Autodesk.Revit.DB;
+using System.Windows.Forms;
 namespace MainProjectApi.ViewSheetAsign
 {
     public class ViewToSheetHandler : IExternalEventHandler
     {
         public void Execute(UIApplication app)
         {
-            throw new NotImplementedException();
+            Document doc = app.ActiveUIDocument.Document;
+            ElementId idSelect = null;
+            try
+            {
+                idSelect  = app.ActiveUIDocument.Selection.GetElementIds().First();
+            }
+            catch
+            {
+                MessageBox.Show("You must choose a view on sheet");
+                return;
+            }
+            Viewport viewPortChoose = doc.GetElement(idSelect) as Viewport;
+            List<Autodesk.Revit.DB.View> listViewSelect = GetViewChecked(doc);
+            List<ViewSheet> listSheetSelect = GetSheetChecked(doc);
+
+
         }
 
         public string GetName()
         {
             return "ViewToSheetHandlers";
+        }
+
+        public List<Autodesk.Revit.DB.View> GetViewChecked(Document doc)
+        {    
+            List<Autodesk.Revit.DB.View> listView = new List<Autodesk.Revit.DB.View>();
+            var listItemView = AppPenalViewToSheet.myFormViewToSheet.listViewViewAssignTo.CheckedItems;
+            var listAllView = new FilteredElementCollector(doc).OfClass(typeof(Autodesk.Revit.DB.View)).Cast<Autodesk.Revit.DB.View>();
+            foreach (ListViewItem view in listItemView)
+            {
+                var name = view.Text;
+                foreach (var viewAdd in listAllView)
+                {
+                    var viewname = viewAdd.ViewType + "/Name: " + viewAdd.Name;
+                    if (viewname == name)
+                    {
+                        listView.Add(viewAdd);
+                    }
+                }
+            }
+            return listView;
+        }
+
+        public List<ViewSheet> GetSheetChecked(Document doc)
+        {
+            var listItemSheet = AppPenalViewToSheet.myFormViewToSheet.listViewSheetAssignView.CheckedItems;
+            var listSheet = new FilteredElementCollector(doc).OfClass(typeof(ViewSheet)).Cast<ViewSheet>();
+            List<ViewSheet> listViewSheetCheck = new List<ViewSheet>();
+            foreach (ListViewItem item in listItemSheet)
+            {
+                var sheet = listSheet.Where(x => x.SheetNumber == item.Text).First();
+                listViewSheetCheck.Add(sheet);
+            }          
+            return listViewSheetCheck;
         }
     }
 }
